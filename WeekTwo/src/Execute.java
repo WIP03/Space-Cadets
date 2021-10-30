0ba23cfdace2b8c8a7c92849f3b27ec675cf29f7
WIP03 <41729427+WIP03@users.noreply.github.com>
2021-10-31 00:04:12 +0100

    While Loop Bugfix
    It fixes a bug that prevented code from executing after all currently passed loops had finished.

--- WeekTwo/src/Execute.java
+++ WeekTwo/src/Execute.java
@@ -4,6 +4,7 @@ public class Execute {
   // *TODO ADD FUNCTIONS FOR EACH FUNCTION TYPES (E.G CLEAR, INCR, DECR, WHILE, END, ECT)
   // TODO THEN CHANGE FUNCTIONS TO OWN CLASSES TO ALLOW FOR ERROR CHECKING AND EASY RECURSION
   // TODO recursive descent for recognising more complex mathematical calculations to work.
+  // use me to fix not reading stuff after list function "|| Main.whileStack.isEmpty()"
 
   // Sets the given variable in the functions' operand to zero.
   public static void clear(String variable) {
@@ -55,13 +56,16 @@ public class Execute {
   // empty then it sets the variable Main.isCurrentLoopEnded to false as a loop must exist. Once
   // these checks occur the Integer storing the value of the current line to execute is returned.
   public static Integer end(Integer lineNum) {
-    // add loop end code here
+    // Removes current loop from stack and sets the new line number base on the position of that
+    // loop in the code.
     if (!Main.isCurrentLoopEnded & !Main.whileStack.isEmpty()) {
       lineNum = Main.whileStack.peek() - 1;
       Main.whileStack.pop();
     }
 
-    if (!Main.whileStack.isEmpty()) {
+    // Allows for function calls to work when loop stack is not empty or is empty but the loop has
+    // ended.
+    if (!Main.whileStack.isEmpty() || (Main.isCurrentLoopEnded & Main.whileStack.isEmpty())) {
       Main.isCurrentLoopEnded = false;
     }
 
