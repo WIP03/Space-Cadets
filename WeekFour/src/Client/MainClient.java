caf5081349037a8d4047d7a9c9e7763583839cfb
WIP03 <41729427+WIP03@users.noreply.github.com>
2021-11-09 10:45:00 +0000

    Initial Setup Week Four
    This comment is for the initial setup of week four of the space cadets challenge.

---
+++ WeekFour/src/Client/MainClient.java
@@ -0,0 +1,8 @@
+package Client;
+
+public class MainClient {
+
+    public static void main(String[] args){
+        System.out.println("Client");
+    }
+}
---
+++ WeekFour/src/Server/MainServer.java
@@ -0,0 +1,8 @@
+package Server;
+
+public class MainServer {
+
+    public static void main(String[] args){
+        System.out.println("Server");
+    }
+}
