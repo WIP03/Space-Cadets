6332b6405043abf61797419c15ef14a2d270aaa6
WIP03 <41729427+WIP03@users.noreply.github.com>
2021-11-12 01:46:39 +0000

    Improved Client Ui
    Changes to client UI to make it more friendly.

--- WeekFour/src/Client/MainClient.java
+++ WeekFour/src/Client/MainClient.java
@@ -10,11 +10,15 @@ public class MainClient {
     System.out.println("##############################################");
     System.out.println("# Please input the IP and Port of the server #");
     System.out.println("##############################################");
+
+
     System.out.print("IP: ");
     Scanner scanner = new Scanner(System.in);
     String Ip = scanner.nextLine();
     System.out.print("Port: ");
     String Port = scanner.nextLine();
+
+    clearConsole(20);
 
     HandlerClientSide client = new HandlerClientSide();
     client.init(Ip, Integer.decode(Port));
@@ -22,11 +26,18 @@ public class MainClient {
     String userInput = "";
 
     while (!userInput.equals("!exit")){
+      System.out.print("Me: ");
       userInput = scanner.nextLine();
 
-      System.out.println(client.sendMessage(userInput));
+      System.out.println("Server: " + client.sendMessage(userInput));
     }
 
     client.stop();
+  }
+
+  public static void clearConsole(int numLines){
+    for (int i = 0; i < numLines; i++){
+      System.out.println();
+    }
   }
 }
