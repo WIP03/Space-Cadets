59fedbed3d5fd91984db1ab1dd69f65dc02162df
WIP03 <41729427+WIP03@users.noreply.github.com>
2021-11-12 00:56:06 +0000

    Custom IP And Port Config
    Allows the client to input any IP or Port of there choice when they load the program.

--- WeekFour/src/Client/MainClient.java
+++ WeekFour/src/Client/MainClient.java
@@ -6,13 +6,22 @@ import java.util.Scanner;
 public class MainClient {
 
   public static void main(String[] args) throws IOException {
+
+    System.out.println("##############################################");
+    System.out.println("# Please input the IP and Port of the server #");
+    System.out.println("##############################################");
+    System.out.print("IP: ");
+    Scanner scanner = new Scanner(System.in);
+    String Ip = scanner.nextLine();
+    System.out.print("Port: ");
+    String Port = scanner.nextLine();
+
     HandlerClientSide client = new HandlerClientSide();
-    client.init("127.0.0.1", 11111);
+    client.init(Ip, Integer.decode(Port));
 
     String userInput = "";
 
     while (!userInput.equals("!exit")){
-      Scanner scanner = new Scanner(System.in);
       userInput = scanner.nextLine();
 
       System.out.println(client.sendMessage(userInput));
